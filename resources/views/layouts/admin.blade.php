6a88a5b83e941cd0bd3e7d4d7301f671ca673e4a
Prashant Shukla <android.prashant.ps@gmail.com>
2024-11-06 00:29:55 +0530

    added app bridge and nav element #2

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -14,20 +14,11 @@
 <body class="bg-gray-100 text-gray-900">
     <div class="flex h-screen">
         <!-- Sidebar -->
-        <aside class="w-64 bg-gray-800 text-white">
-            <div class="p-6">
-                <h2 class="text-xl font-semibold">Admin Panel</h2>
-            </div>
-
-            <ui-nav-menu  class="p-4">
-                <a href="/" rel="home">Home</a>
-                <a href="/templates">Templates</a>
-                <a href="/settings">Settings</a>
-                <a href="{{ route('admin.dashboard') }}" class="block py-2 px-4 text-gray-300 hover:bg-gray-700">Dashboard</a>
-                <a href="{{ route('admin.products') }}" class="block py-2 px-4 text-gray-300 hover:bg-gray-700">Products</a>
-            </ui-nav-menu>
-            
-        </aside>
+        <ui-nav-menu>
+            <a href="/" rel="home">Home</a>
+            <a href="{{ route('admin.dashboard') }}" class="block py-2 px-4 text-gray-300 hover:bg-gray-700">Dashboard</a>
+            <a href="{{ route('admin.products') }}" class="block py-2 px-4 text-gray-300 hover:bg-gray-700">Products</a>
+        </ui-nav-menu>
 
         <!-- Content -->
         <main class="flex-1 p-6">
