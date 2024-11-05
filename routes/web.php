c91a6399a674fa2c8ebfd73592402eef9270a525
Prashant Shukla <android.prashant.ps@gmail.com>
2024-11-05 20:59:18 +0530

    added app bridge#3

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 
-// Route::middleware(['verify.shopify'])->group(function () {
+Route::middleware(['verify.shopify'])->group(function () {
     Route::get('/', [AdminController::class, 'dashboard'])->name('admin.dashboard');
     Route::get('/products', [AdminController::class, 'products'])->name('admin.products');
-// });
+});
 
 
 
