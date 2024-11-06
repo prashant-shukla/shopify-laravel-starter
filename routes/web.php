9a8b8eeb3ad60a5fdcb2b097aff757094c9c64ac
Prashant Shukla <android.prashant.ps@gmail.com>
2024-11-06 23:34:20 +0530

    removed QR code

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\QuestionsController;
-use App\Http\Controllers\QRCodeController;
 
 
 Route::middleware(['verify.shopify'])->group(function () {
@@ -13,18 +12,5 @@ Route::middleware(['verify.shopify'])->group(function () {
     Route::get('/add-questions', [QuestionsController::class, 'addQuestions'])->name('add-questions');
 
     Route::post('/save-questions', [QuestionsController::class, 'saveQuestions'])->name('save-questions');
-
-    // Show form for creating a new QR code or editing an existing one
-    Route::get('/qrcodes/{id?}', [QRCodeController::class, 'showForm'])->name('qrcodes.form');
-
-    // Handle creation and update actions for QR codes
-    Route::post('/qrcodes', [QRCodeController::class, 'handleAction'])->name('qrcodes.store'); // Route for creating
-    Route::post('/qrcodes/{id}', [QRCodeController::class, 'handleAction'])->name('qrcodes.update'); // Route for updating
-
-    // Route for selecting a product
-    Route::post('/qrcodes/select-product', [QRCodeController::class, 'selectProduct'])->name('qrcodes.selectProduct');
-
-    // Route for deleting a QR code
-    Route::delete('/qrcodes/{id}', [QRCodeController::class, 'handleAction'])->name('qrcodes.destroy');
 });
 
