db5c89d1c27ae19071ac8b17c4fdc22889169a2f
Prashant Shukla <android.prashant.ps@gmail.com>
2024-11-07 13:27:43 +0530

    added shopify/shopify-api #2

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,12 @@ use App\Http\Controllers\ShopifyAuthController;
 
 Route::get('/auth', [ShopifyAuthController::class, 'redirectToShopify']);
 Route::get('/auth/callback', [ShopifyAuthController::class, 'handleCallback']);
-
+Route::get('/', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+Route::get('/products', [AdminController::class, 'products'])->name('admin.products');
 
 Route::middleware(['verify.shopify'])->group(function () {
-    Route::get('/', [AdminController::class, 'dashboard'])->name('admin.dashboard');
-    Route::get('/products', [AdminController::class, 'products'])->name('admin.products');
     Route::get('/questions', [QuestionsController::class, 'showQuestions'])->name('questions');
     Route::get('/add-questions', [QuestionsController::class, 'addQuestions'])->name('add-questions');
-
     Route::post('/save-questions', [QuestionsController::class, 'saveQuestions'])->name('save-questions');
 });
 
