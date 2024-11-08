8310ad0b462a76f56f6050500b0f267eebe525b0
Prashant Shukla <android.prashant.ps@gmail.com>
2024-11-08 12:30:28 +0530

    added home route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\QuestionsController;
 
 Route::middleware(['verify.shopify'])->group(function () {
     Route::get('/', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::get('/home', [AdminController::class, 'dashboard'])->name('home');
     Route::get('/questions', [QuestionsController::class, 'showQuestions'])->name('questions');
     Route::get('/add-questions', [QuestionsController::class, 'addQuestions'])->name('add-questions');
     Route::post('/save-questions', [QuestionsController::class, 'saveQuestions'])->name('save-questions');
