16b2cf4f6dda47d0ee105ea4201dd321b045b6c8
Prashant Shukla <android.prashant.ps@gmail.com>
2024-11-08 12:23:18 +0530

    Revert to commit 0b17b34

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\QuestionsController;
 
+
 Route::middleware(['verify.shopify'])->group(function () {
     Route::get('/', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::get('/products', [AdminController::class, 'products'])->name('admin.products');
     Route::get('/questions', [QuestionsController::class, 'showQuestions'])->name('questions');
     Route::get('/add-questions', [QuestionsController::class, 'addQuestions'])->name('add-questions');
+
     Route::post('/save-questions', [QuestionsController::class, 'saveQuestions'])->name('save-questions');
-    Route::get('/products', [AdminController::class, 'products'])->name('admin.products');
 });
 
